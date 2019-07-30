8c1b0181807fe9277fb8454b876430369dbbe987
Evan Bowling <evan.j.bowling@gmail.com>
2019-07-29 23:35:25 -0500

    Update `nth-rational` and `rational-seq`
    - both use a 0-based index now (even though the
      underlying private `nth-rational*` uses a 1-
      based index for convenience/clarity

--- src/com/evanjbowling/rational.clj
+++ src/com/evanjbowling/rational.clj
@@ -1,34 +1,35 @@
 (ns com.evanjbowling.rational)
 
-(defn ^:private nth-rat
-  "Returns the nth rational number as a two element
-  vector [numerator denominator] by navigating the
-  Calkin-Wilf tree."
+(defn ^:private nth-rational*
+  "Returns the nth rational number (1-based) as a
+  two element vector [numerator denominator] by
+  navigating the Calkin-Wilf tree."
   [n]
   (if (< n 2)
     [1 1]
-    (let [[pn pd] (nth-rat (/ (cond-> n (odd? n) dec) 2))
+    (let [[pn pd] (nth-rational* (/ (cond-> n (odd? n) dec) 2))
           ps (+' pn pd)]
       (cond
         (odd? n) [ps pd]
         :else    [pn ps]))))
 
 (defn nth-rational
-  "Returns the nth rational number."
-  [n]
-  (when (or (not (integer? n)) (< n 1))
-    (throw (IllegalArgumentException. "n is not an integer > 0")))
-  (let [[rn rd] (nth-rat n)]
+  "Returns the rational number at the index
+  specified (0-based). Throws for non-integer
+  and non-positive indices."
+  [index]
+  (when (or (not (integer? index)) (< index 0))
+    (throw (IllegalArgumentException. "index is not an integer >= 0")))
+  (let [[rn rd] (nth-rational* (inc index))]
     (/ rn rd)))
 
-(defn ^:private rat-seq*
-  [n]
-  (lazy-seq
-   (let [[rn rd] (nth-rat n)]
-     (cons (/ rn rd) (rat-seq* (inc n))))))
-
 (defn rational-seq
-  "Returns infinite lazy sequence of all rational
-  numbers as ordered in the Calkin-Wilf tree."
-  [] (rat-seq* 1))
+  "Returns infinite lazy sequence of rational numbers.
+  First form returns all positive rational numbers.
+  Second form starts at the specified index (0-based)."
+  ([] (rational-seq 0))
+  ([n]
+   (lazy-seq
+    (let [[rn rd] (nth-rational* (inc n))]
+      (cons (/ rn rd) (rational-seq (inc n)))))))
 
--- test/com/evanjbowling/rational_test.clj
+++ test/com/evanjbowling/rational_test.clj
@@ -9,7 +9,9 @@
           1/3 3/2 2/3 3
           1/4 4/3 3/5 5/2 2/5 5/3 3/4 4
           1/5 5/4 4/7 7/3 3/8 8/5 5/7 7/2 2/7 7/5 5/8 8/3 3/7 7/4 4/5 5]
-         (take 31 (r/rational-seq)))))
+         (take 31 (r/rational-seq))
+         (map r/nth-rational (range 31)))
+      "validates values, ordering and 0-based index"))
 
 (deftest test-no-duplicates
   (is (->> (take 100 (r/rational-seq))
@@ -20,18 +22,19 @@
 (deftest test-large-n
   (is (= 205/162
          (nth (r/rational-seq) 10000)
-         (r/nth-rational 10001)))
-  (is (= 2142134094734576905/100081852181957676623
+         (r/nth-rational 10000)))
+  (is (= 100081852181957676623/97939718087223099718
          (r/nth-rational 10000000000000000000000000000000000000000000000))
       "can handle large n such that the denominator is larger than Long/MAX_VALUE"))
 
 (deftest test-invalid-inputs
   (are [n] (thrown-with-msg?
-             IllegalArgumentException
-             #"^n is not an integer > 0.*$"
-             (r/nth-rational n))
-    0
-   -5
+            IllegalArgumentException
+            #"^index is not an integer >= 0.*$"
+            (r/nth-rational n))
+    -1
+    -5
+    1M
     1.1M
     0.25
     nil
